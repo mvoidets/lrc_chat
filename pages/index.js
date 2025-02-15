603ac2bc96fd81df32536aab16c1aa18066d58e4
mvoidets <mvoidets@yahoo.com>
2025-02-15 11:46:31 -0700

    mod index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
 export default function Home() { 
-     return <h1>Welcome to <a href="https://flex-dice.vercel.app/"> LRC</a></h1> }
+     return <h1>Welcome to <a href="https://lrcgame.vercel.app/"> LRC</a></h1> }
 
